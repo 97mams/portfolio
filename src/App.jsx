aae4089a7e8f9ac7f669640ee9e4155fb4d906a3
97mams <97mams@gmail.com>
2024-04-03 15:56:05 +0300

    add projects component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Header } from "./components/Header.jsx";
 import { About } from "./components/About.jsx";
 import { Skills } from "./components/Skills.jsx";
+import { Projects } from "./components/Projects.jsx";
 
 export default function App() {
-  return <div className="w-full min-h-screen bg-blue-900 pt-5">
+  return <div className="w-full min-h-screen bg-blue-900 pt-5 px-20">
     <Header></Header>
     <About></About>
     <Skills></Skills>
+    <Projects></Projects>
   </div>
 }
---
+++ src/components/Projects.jsx
@@ -0,0 +1,19 @@
+export const Projects = () => {
+    return <div id="projects">
+        <h1 className="text-5xl text-white text-center font-bold my-10">Projects</h1>
+        <div className="flex justify-around pb-20 px-5">
+            <div className="w-[300px] p-5 border border-blue-800 rounded-md">
+                <div className="h5">Card title</div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque expedita qui illo sint esse fuga labore error </p>
+            </div>
+            <div className="w-[250px] p-5 border border-blue-800 rounded-md">
+                <div className="h5">Card title</div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque expedita qui illo sint esse fuga labore error </p>
+            </div>
+            <div className="w-[250px] p-5 border border-blue-800 rounded-md">
+                <div className="h5">Card title</div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque expedita qui illo sint esse fuga labore error </p>
+            </div>
+        </div>
+    </div>
+}
